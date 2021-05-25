b30e369cc677818da6ef4574ce09a278566f4a5b
Kaveeshwa Bhashitha <kaveeshwa.b@informaticsint.com>
2021-05-25 15:09:14 +0530

    home added and background image added

---
+++ packages/server-api/dist/index.modern.js
@@ -0,0 +1,13 @@
+import { createElement } from 'react';
+
+var styles = {"test":"_3ybTi"};
+
+var ExampleComponent = function ExampleComponent(_ref) {
+  var text = _ref.text;
+  return createElement("div", {
+    className: styles.test
+  }, "test : ", text);
+};
+
+export { ExampleComponent };
+//# sourceMappingURL=index.modern.js.map
---
+++ packages/weather-app/src/components/TopBar/TopBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopBar() {
+    return (
+        <div>
+            <h1>Weather App</h1>
+        </div>
+    )
+}
+
+export default TopBar
